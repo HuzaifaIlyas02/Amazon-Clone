9ff228203619f6471e0a72e7d2109cb62e55b1b4
HuzaifaIlyas02 <huzaifailyas02@outlook.com>
2024-02-12 22:41:40 +0500

    Completed Home Page implementation

--- amazon-clone/src/Components/Home/Home.js
+++ amazon-clone/src/Components/Home/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Home.css";
+import Product from "../Product/Product";
 
 function Home() {
   return (
@@ -12,17 +13,55 @@ function Home() {
         />
 
         <div className="home_row">
-          {/* Product */}
-          {/* Product */}
+          <Product
+            id="12321341"
+            title="The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback"
+            price={29.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg"
+            rating={5}
+          />
+          <Product
+            id="49538094"
+            title="Kenwood kMix Stand Mixer for Baking, Stylish Kitchen Mixer with K-beater, Dough Hook and Whisk, 5 Litre Glass Bowl"
+            price={239.0}
+            image="https://images-na.ssl-images-amazon.com/images/I/81O%2BGNdkzKL._AC_SX450_.jpg"
+            rating={4}
+          />
         </div>
 
         <div className="home_row">
-          {/* Product */}
-          {/* Product */}
-          {/* Product */}
+          <Product
+            id="4903850"
+            title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor"
+            price={199.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/71Swqqe7XAL._AC_SX466_.jpg"
+            rating={3}
+          />
+          <Product
+            id="23445930"
+            title="Amazon Echo (3rd generation) | Smart speaker with Alexa, Charcoal Fabric"
+            price={98.99}
+            image="https://m.media-amazon.com/images/I/61b4qFTXRML._AC_UY218_.jpg"
+            rating={5}
+          />
+          <Product
+            id="3254354345"
+            title="New Apple iPad Pro (12.9-inch, Wi-Fi, 128GB) - Silver (4th Generation)"
+            price={598.99}
+            image="https://m.media-amazon.com/images/I/81FH2j7EnJL._AC_UY218_.jpg"
+            rating={4}
+          />
         </div>
 
-        <div className="home_row">{/*Product*/}</div>
+        <div className="home_row">
+          <Product
+            id="90829332"
+            title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor"
+            price={1094.98}
+            image="https://images-na.ssl-images-amazon.com/images/I/6125mFrzr6L._AC_SX355_.jpg"
+            rating={4}
+          />
+        </div>
       </div>
     </div>
   );
--- amazon-clone/src/Components/Product/Product.js
+++ amazon-clone/src/Components/Product/Product.js
@@ -1,12 +1,27 @@
 import React from "react";
 import "./Product.css";
 
-function Product() {
+function Product({ id, title, image, price, rating }) {
   return (
     <div className="product">
       <div className="product_info">
-        <p>The Lean Startup</p>
+        <p>{title}</p>
+        <p className="product_price">
+          <small>$</small>
+          <strong>{price}</strong>
+        </p>
+        <div className="product_rating">
+          {Array(rating)
+            .fill()
+            .map((_, i) => {
+              return <p>⭐</p>;
+            })}
+        </div>
       </div>
+
+      <img src={image} alt="" />
+
+      <button>Add to Basket</button>
     </div>
   );
 }
