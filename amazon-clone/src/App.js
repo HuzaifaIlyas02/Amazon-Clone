9d3230de88500aa4c8ebd1d4ee9cf06ca19e2bf0
HuzaifaIlyas02 <huzaifailyas02@outlook.com>
2024-02-13 19:02:51 +0500

    Fixed Issues of react-router-dom

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -2,14 +2,25 @@ import React from "react";
 import "./App.css";
 import Header from "./Components/Header/Header";
 import Home from "./Components/Home/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    // BEM naming convention
-    <div className="app">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/checkout">
+            <Header />
+            <h1>I am a checkout</h1>
+            {/* <Home /> */}
+          </Route>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
